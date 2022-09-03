a99499d2693e76d8790558e1a9ccde1b41dafa3d
James Bachini <james@mcambridge.com>
2022-09-03 21:22:38 +0100

    Create encodeCall.sol

---
+++ contracts/encodeCall.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.16;
+// available from 0.8.11
+
+interface IERC20 {
+	function transfer(address recipient, uint amount) external returns (bool);
+}
+
+contract encodeCall {
+    function encodeCallData(address _to, uint _value) public pure returns (bytes memory) {
+        return abi.encodeCall(IERC20.transfer, (_to, _value));
+    }
+}
