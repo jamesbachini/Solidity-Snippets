c690083748745fcfbb578048f66c4832b2244366
James Bachini <james@mcambridge.com>
2022-12-28 11:07:23 +0000

    Array del by index

--- contracts/Arrays.sol
+++ contracts/Arrays.sol
@@ -4,7 +4,7 @@ pragma solidity >=0.8.0;
 // Arrays Example
 contract Arrays {
 
-    uint[] public myArray;
+    uint[] public myArray = [2,4,6,8,10,12];
     uint[4] public myFixedArray = [3,5,1,8];
     uint[][] public nestedArray;
 
@@ -20,5 +20,12 @@ contract Arrays {
 
     function arrayLength() public view returns(uint) {
         return myArray.length;
+    }
+
+    function delByIndex(uint _i) public {
+        for (uint i = _i; i < myArray.length - 1; ++i) {
+            myArray[i] = myArray[i+1];
+        }
+        myArray.pop();
     }
 }
