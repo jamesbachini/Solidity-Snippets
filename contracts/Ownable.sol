8094f903c4771bfc6dad6a28970ae723330da7db
James Bachini <james@mcambridge.com>
2023-03-16 14:59:09 +0000

    Update Ownable.sol

--- contracts/Ownable.sol
+++ contracts/Ownable.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0;
 
-import "@openzeppelin/contracts//contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract OwnableExample1 is Ownable {
     function adminFunctionExample() public onlyOwner {
