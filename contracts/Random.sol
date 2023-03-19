247239bd4b5529ee2f2de1c563a8a0442217fb39
James Bachini <james@mcambridge.com>
2023-03-19 07:56:02 +0000

    prevrandao

--- contracts/Random.sol
+++ contracts/Random.sol
@@ -1,10 +1,15 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.8.0;
+pragma solidity >=0.8.18;
 
 // Generate a random enough number for most applications
 contract RandomishNumbers {
 
-    function random() private view returns (uint) {
+    function newRandom() private view returns (uint) {
+        uint randomNumber = block.prevrandao;
+        return randomNumber;
+    }
+
+    function oldRandom() private view returns (uint) {
         uint hashNumber =  uint(keccak256(abi.encodePacked(block.difficulty, block.timestamp, msg.sender)));
         return hashNumber % 100;
     }
